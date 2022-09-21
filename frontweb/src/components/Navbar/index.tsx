ee7ac53ae41e466189525f02cdd629032b3059cf
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-21 19:35:49 -0300

    Navbar styles

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,15 +1,18 @@
+
+import './styles.css';
+
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-primary">
-        <div>
-          <a href="link">
-            <h4>DS Catalog</h4>
+      <nav className="navbar navbar-expand-md bg-primary main-nav">
+        <div className="container-fluid">
+          <a href="link" className="nav-log-text">
+            <h4>TS Catalog</h4>
           </a>
-          <div>
-            <ul>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav offset-md-2 main-menu">
               <li>
-                <a href="link">HOME</a>
+                <a href="link" className="active">HOME</a>
               </li>
               <li>
                 <a href="link">CATÁLOGO</a>
