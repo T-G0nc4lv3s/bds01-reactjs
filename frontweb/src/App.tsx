7ae740399324c64d0e6417db7cdf6c6253c88ca7
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-21 18:07:16 -0300

    Navbar

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,8 +1,14 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <h1 >Hello TScatalog</h1>;
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1>Hello TScatalog</h1>
+    </>
+  );
 }
 
 export default App;
