5b24e019c112a419add8ba7bc722ad1bc017ce2b
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-21 20:56:43 -0300

    Home page basic HTML

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-      <h1>Hello TScatalog</h1>
+      <Home />
     </>
   );
 }
