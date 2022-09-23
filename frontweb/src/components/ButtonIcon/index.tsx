526aee36f1499ebdc753d0afe95bdf1119eb4147
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-23 16:56:36 -0300

    Custom ButtonIcon

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,11 +1,18 @@
 import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/images/Arrow.svg';
 
 const ButtonIcon = () => {
-    return (
-        <button className="btn btn-primary btn-icon">
-            <h6>Inicie agora a sua busca</h6>
-        </button>
-    );
-}
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>Inicie agora a sua busca</h6>
+      </button>
+
+      <div className="btn-icon-container">
+        <ArrowIcon />
+      </div>
+    </div>
+  );
+};
 
 export default ButtonIcon;
