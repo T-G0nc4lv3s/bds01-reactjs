2ae19e387334ff5f9c52aa4809028b3b37573560
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-04 11:02:12 -0300

    FormatPrice

---
+++ frontweb/src/util/formatters.ts
@@ -0,0 +1,7 @@
+
+export const formatPrice = (price: number) => {
+
+    const params = {minimumFractionDigits: 2, maximumFractionDigits: 2};
+
+    return Intl.NumberFormat('pt-BR', params).format(price);
+}
