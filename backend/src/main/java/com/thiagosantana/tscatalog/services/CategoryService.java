0c2f91bac3de945ea813bc2ae71a90aebc9f599e
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-03 21:39:12 -0300

    Back end

---
+++ backend/src/main/java/com/thiagosantana/tscatalog/services/CategoryService.java
@@ -0,0 +1,75 @@
+package com.thiagosantana.tscatalog.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.thiagosantana.tscatalog.dto.CategoryDTO;
+import com.thiagosantana.tscatalog.entities.Category;
+import com.thiagosantana.tscatalog.repositories.CategoryRepository;
+import com.thiagosantana.tscatalog.services.exceptions.DatabaseException;
+import com.thiagosantana.tscatalog.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAllPaged(Pageable pageable){	
+		Page<Category> list = repository.findAll(pageable);
+		return list.map(cat -> new CategoryDTO(cat));
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
+}
---
+++ backend/src/test/java/com/thiagosantana/tscatalog/tests/Factory.java
@@ -0,0 +1,26 @@
+package com.thiagosantana.tscatalog.tests;
+
+import java.time.Instant;
+
+import com.thiagosantana.tscatalog.dto.ProductDTO;
+import com.thiagosantana.tscatalog.entities.Category;
+import com.thiagosantana.tscatalog.entities.Product;
+
+public class Factory {
+
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+		product.getCategories().add(createCategory());
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+	
+	public static Category createCategory() {
+		return new Category(1L, "Electronics");
+	}
+}
