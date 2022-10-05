b4ce0c72c62a2bca2aabb449bf5a5c15e4e3249a
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-05 09:38:38 -0300

    Admin, Navbar html

---
+++ frontweb/src/pages/Admin/Navbar/index.tsx
@@ -0,0 +1,19 @@
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Produtos</li>
+            </ul>
+            <ul>
+                <li>Categorias</li>
+            </ul>
+            <ul>
+                <li>Usuários</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
