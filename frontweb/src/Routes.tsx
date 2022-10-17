22d428fcb664b8a7d178fc0d6abe53d5f16ce5cc
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-17 14:52:09 -0300

    Auth routes

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -20,6 +20,7 @@ const Routes = () => (
       <Route path="/products/:productId">
         <ProductDetails />
       </Route>
+      <Redirect exact from="/admin/auth" to="/admin/auth/login" />
       <Route path="/admin/auth">
         <Auth />
       </Route>
--- frontweb/src/pages/Admin/Auth/index.tsx
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -1,10 +1,35 @@
 import './styles.css';
+import { ReactComponent as AuthImg } from 'assets/images/auth-image.svg';
+import { Switch, Route } from 'react-router-dom';
 
 const Auth = () => {
-
-    return (
-        <h1>Tela Auth</h1>
-    );
-}
+  return (
+    <div className="auth-container">
+      <div className="auth-banner-container">
+        <h1>Divulgue seus produtos no Ts catalog</h1>
+        <p>
+          Faça parte do nosso catálogo de divulgação e aumente a venda dos seus
+          produtos.
+        </p>
+        <div>
+          <AuthImg />
+        </div>
+      </div>
+      <div className="form-container">
+        <Switch>
+          <Route path="/admin/auth/login">
+            <h1>Card de login</h1>
+          </Route>
+          <Route path="/admin/auth/signup">
+            <h1>Card de signup</h1>
+          </Route>
+          <Route path="/admin/auth/recover">
+            <h1>Card de recover</h1>
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
