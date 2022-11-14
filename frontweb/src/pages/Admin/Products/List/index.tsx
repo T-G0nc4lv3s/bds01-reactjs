679c304cd9aa58e46078051a61420f8cbaf1c353
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-11-14 16:27:48 -0300

    CategoryBadge

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,7 +1,6 @@
 import './styles.css';
 import { Link } from 'react-router-dom';
-
-import ProductCrudCard from 'components/ProductCrudCard';
+import ProductCrudCard from './ProductCrudCard';
 
 const List = () => {
   const product = {
@@ -17,6 +16,10 @@ const List = () => {
       {
         id: 2,
         name: 'Eletrônicos',
+      },
+      {
+        id: 1,
+        name: 'Computadores',
       },
     ],
   };
