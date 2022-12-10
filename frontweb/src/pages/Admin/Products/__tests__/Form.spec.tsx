7acb46c51147d169a82fa0931b51aca5ed49e3f2
Thiago Santana <mg.az.ul@hotmail.com>
2022-12-10 16:35:28 -0300

    Mock React Router DOM e useParams

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -2,13 +2,27 @@ import { render, screen } from '@testing-library/react';
 import Form from '../Form';
 import { Router } from 'react-router-dom';
 import history from 'util/history';
+import { useParams } from 'react-router-dom';
 
-test('should render Form', () => {
-  render(
-    <Router history={history}>
-      <Form />
-    </Router>
-  );
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn(),
+}));
 
-  screen.debug();
+describe('Product form create tests', () => {
+  beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({
+      productId: 'create',
+    });
+  });
+
+  test('should render Form', () => {
+    render(
+      <Router history={history}>
+        <Form />
+      </Router>
+    );
+
+    screen.debug();
+  });
 });
